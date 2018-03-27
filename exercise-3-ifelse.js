e0b8aefaadf4b1ba4ccc7880cd9303f9443b8261
Rhesa Utomo <venderblitz@gmail.com>
2018-03-27 17:24:57 +0700

    exercise 3 finished

---
+++ exercise-3-ifelse.js
@@ -0,0 +1,16 @@
+var nama = '';
+var peran = '';
+
+if (nama === '') {
+  console.log("Nama harus diisi!")
+} else {
+  if (peran === 'Ksatria') {
+    console.log("Halo Ksatria " + nama + ", kamu dapat menyerang dengan senjatamu!")
+  } else if (peran === 'Tabib') {
+    console.log("Halo Tabib " + nama + ", kamu akan membantu temanmu yang terluka.")
+  } else if (peran === 'Penyihir') {
+    console.log("Halo Penyihir " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!")
+  } else {
+    console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!")
+  }
+}
