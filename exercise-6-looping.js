b637c2ee1c62a6416a66e862d544f4aa025eaccb
Rhesa Utomo <venderblitz@gmail.com>
2018-03-29 14:56:12 +0700

    exercise 6 finished

--- exercise-6-looping.js
+++ exercise-6-looping.js
@@ -37,21 +37,21 @@ for (var i = 1; i <= 10; i++) {
 
 console.log('LOOPING KELIPATAN 3 (counter += 2)')
 for (var i = 1; i <= 100; i += 2) {
-  if (i % 3 == 0) {
+  if (i % 3 === 0) {
     console.log(i + ' KELIPATAN 3')
   }
 }
 
 console.log('LOOPING KELIPATAN 6 (counter += 5)')
 for (var i = 1; i <= 100; i += 5) {
-  if (i % 6 == 0) {
+  if (i % 6 === 0) {
     console.log(i + ' KELIPATAN 6')
   }
 }
 
 console.log('LOOPING KELIPATAN 10 (counter += 9)')
 for (var i = 1; i <= 100; i += 9) {
-  if (i % 10 == 0) {
+  if (i % 10 === 0) {
     console.log(i + ' KELIPATAN 10')
   }
 }
